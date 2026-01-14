c22711596391da28db0a31bb1f2858c44dcd0cdb
TonyTec42 <acorraleszapata1072@floridapoly.edu>
2026-01-14 18:52:55 +0000

    First Day cODING

---
+++ 01_first_program/first_program.cpp
@@ -0,0 +1,29 @@
+//the first c++ program
+//1/14/2026
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+using std::cout;
+
+int main(void){
+
+    //cd.. goes to the parent folder
+
+
+    //print to the console
+    std::cout << "Hello from COP3337 \n";
+    std::cout << "Hello from COP3337 \n" << std::endl;
+
+
+    std::cout << 87 + 76 << std::endl;
+    //standard namespace (adiditonal container where you cna define classes or variables)
+    // cout = cosnole output (object of iosstream class)
+    // == stream insertion operator
+    //endl = end of line
+
+
+
+    return 0;
+
+}

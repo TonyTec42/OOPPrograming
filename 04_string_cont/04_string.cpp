666243112f3b924437759d9112660b896d4a2f41
TonyTec42 <acorraleszapata1072@floridapoly.edu>
2026-01-21 18:49:30 +0000

    end of class v2 1/21

--- 04_string_cont/04_string.cpp
+++ 04_string_cont/04_string.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+const double PI = 3.14;
 
+//constants are usually all capital letters
 int main(){
     std::string str1 = "This is first string";
 
@@ -17,8 +19,36 @@ int main(){
     
     if(str1 == str2){
         std::cout << str4 + str1 << std::endl;
-        
+
     }
+
+    std::string strt1 = "ABCD";
+    std::string strt2 = "ABCE";
+
+    std::cout << (strt1 > strt2) << std::endl;
+    //this outputs 0 because 'D' has a lower ASCII value than 'E'
+
+
+
+
+    //short form of the if statement
+    //std::string s0 = s0("3");
+    std::string s0 = "3";
+    std::string s1 = "8";
+    std::string s2 = s0 + s1;
+    s2 += '7';
+
+     s2 += (s1 < s2) ? s0 : "Z"; //if s1 is less than s2, append s0 to s2, else append "Z"
+    std::cout << s2 << "J" << std::endl;
+        //equality test of to floating point values??
+
+        //conditional expression;
+     int x = 75, y= 75;
+     
+    
+        y = (x > 50) ? 50 : x;
+
+
 
     return 0;
 }
